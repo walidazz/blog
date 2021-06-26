b3c34234b66419748a8d389ae411caa18a032136
Walid Azzimani <walidazzimani@gmail.com>
2021-06-26 18:05:19 +0200

    fixtures + fix bug card

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comments;
+use Cocur\Slugify\Slugify;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
@@ -19,6 +23,8 @@ class AppFixtures extends Fixture
     }
     public function load(ObjectManager $manager)
     {
+
+        $slugify = new Slugify();
         // $product = new Product();
         // $manager->persist($product);
         $admin = new User();
@@ -26,6 +32,47 @@ class AppFixtures extends Fixture
             ->setPassword($this->encoder->encodePassword($admin, 'sharingan.'))
             ->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
+
+
+
+        $tabCategory = ['Actualité', 'Developpement', 'Jeux-Vidéos'];
+
+        for ($i = 0; $i < count($tabCategory); $i++) {
+            $category = new Category();
+            $category->setLibelle($tabCategory[0 + $i]);
+            $manager->persist($category);
+        }
+
+        for ($j = 0; $j < 10; $j++) {
+            # code...
+            $article = new Article();
+            $article->setTitre('article numéro ' . $i)
+                ->setContent('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo, nesciunt distinctio ratione facilis,         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+                doloribus eius nisi nostrum voluptas earum quasi         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+                eaque quam! Omnis officiis quae id beatae voluptatem voluptas.
+            ')
+                ->setCoverImage('symfony.png')
+                ->setReadTime(mt_rand(1, 10))
+                ->setCreatedAt(new \DateTime('now'))
+                ->setSlug($slugify->slugify($article->getTitre()))
+                ->setCategory($category);
+            $manager->persist($article);
+        }
+
+        $comment = new Comments();
+        $comment->setEmail('test@gmail.com')
+            ->setPseudo('test@gmail.com')
+            ->setActive(true)
+            ->setContent('Lorem ipsum dolor, sit amet consectetur         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+            adipisicing elit. Asperiores ratione, velit ducimus sunt praesentium voluptatum animi! Aut explicabo atque, corporis, molestias officiis numquam quam reprehenderit rerum voluptas illo suscipit itaque?
+        ')
+            ->setArticle($article)
+            ->setCreatedAt(new \DateTime('now'));
+        $manager->persist($article);
+
         $manager->flush();
     }
 }
